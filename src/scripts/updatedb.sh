367bf4013e317a377e8c7755d529f848c047dbeb
SUZUKI Kei <todokimasen@gmail.com>
2023-12-23 09:17:26 +0900

    正常に動作しない状況を減らすために以下の変更をおこなった.
     * set -o pipefail, shopt -s inherit_errexit を追加した.
     * グローバル変数を宣言するときは明示的に declare に -g を指定するように変更した.
     * パスに "-" が含まれていても動作するように修正した.

--- src/scripts/updatedb.sh
+++ src/scripts/updatedb.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-set -eu
+set -eu -o pipefail
 
-declare -r ROOT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
-declare -r DATA_DIR="${ROOT_DIR}/data"
-declare -r COMPANY_HOLIAYS_TSV_FILE_PATH="${DATA_DIR}/company-holidays.tsv"
-declare -r JAPANESE_HOLIAYS_TSV_FILE_PATH="${DATA_DIR}/japanese-holidays.tsv"
+# コマンド置換したときに errexit の値を継承する.
+shopt -s inherit_errexit
+
+declare ROOT_DIR
+ROOT_DIR="$(cd -- "$(dirname -- "$0")/../.." && pwd)"
+declare -gr ROOT_DIR="${ROOT_DIR}"
+
+declare -gr DATA_DIR="${ROOT_DIR}/data"
+declare -gr COMPANY_HOLIAYS_TSV_FILE_PATH="${DATA_DIR}/company-holidays.tsv"
+declare -gr JAPANESE_HOLIAYS_TSV_FILE_PATH="${DATA_DIR}/japanese-holidays.tsv"
 
 function main
 {
@@ -15,7 +21,7 @@ function main
 
 function prepare_company_holidays_tsv_file
 {
-    mkdir -p "$(dirname "${COMPANY_HOLIAYS_TSV_FILE_PATH}")"
+    mkdir -p "$(dirname -- "${COMPANY_HOLIAYS_TSV_FILE_PATH}")"
 
     declare -r year_from=1955
     declare -r year_to=$(date --date '1 year' '+%Y')
@@ -33,7 +39,7 @@ function prepare_company_holidays_tsv_file
 
 function prepare_japanese_holidays_tsv_file
 {
-    mkdir -p "$(dirname "${JAPANESE_HOLIAYS_TSV_FILE_PATH}")"
+    mkdir -p "$(dirname -- "${JAPANESE_HOLIAYS_TSV_FILE_PATH}")"
 
     curl -sS 'https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv' \
         | iconv --from-code=Shift_JIS --to-code=UTF-8 \
